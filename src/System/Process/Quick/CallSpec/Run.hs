77231e9a4698a342f8689ba2b5f0706dbad51fa3
Daniil Iaitskov <dyaitskov@gmail.com>
2025-06-01 10:34:40 -0800

    master exand haddock import aliases in Run

--- src/System/Process/Quick/CallSpec/Run.hs
+++ src/System/Process/Quick/CallSpec/Run.hs
@@ -4,22 +4,22 @@ import System.Process.Quick.Prelude
 import System.Process.Quick.CallSpec.Type
 import System.Process qualified as SP
 
--- | See 'SP.callProcess'
+-- | See 'System.Process.callProcess'
 callProcess :: (MonadIO m, CallSpec cs) => cs -> m ()
 callProcess cs = liftIO $ SP.callProcess (programName $ pure cs) (programArgs cs)
 
--- | See 'SP.spawnProcess'
+-- | See 'System.Process.spawnProcess'
 spawnProcess :: (MonadIO m, CallSpec cs) => cs -> m SP.ProcessHandle
 spawnProcess cs = liftIO $ SP.spawnProcess (programName $ pure cs) (programArgs cs)
 
--- | See 'SP.readProcess'
+-- | See 'System.Process.readProcess'
 readProcess :: (MonadIO m, CallSpec cs) => cs -> String -> m String
 readProcess cs input = liftIO $ SP.readProcess (programName $ pure cs) (programArgs cs) input
 
--- | See 'SP.readProcessWithExitCode'
+-- | See 'System.Process.readProcessWithExitCode'
 readProcessWithExitCode :: (MonadIO m, CallSpec cs) => cs -> String -> m (ExitCode, String, String)
 readProcessWithExitCode cs input = liftIO $ SP.readProcessWithExitCode (programName $ pure cs) (programArgs cs) input
 
--- | See 'SP.proc'
+-- | See 'System.Process.proc'
 proc :: CallSpec cs => cs -> CreateProcess
 proc cs = SP.proc (programName $ pure cs) (programArgs cs)
