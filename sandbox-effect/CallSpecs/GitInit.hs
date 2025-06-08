230540269d295934995b9246ce2623aea82256af
Daniil Iaitskov <dyaitskov@gmail.com>
2025-06-07 16:25:49 -0800

    master git init with default branch specified to avoid pesky warning

--- sandbox-effect/CallSpecs/GitInit.hs
+++ sandbox-effect/CallSpecs/GitInit.hs
@@ -1,13 +1,14 @@
 -- {-# OPTIONS_GHC -ddump-splices #-}
 module CallSpecs.GitInit where
 
+import Data.String qualified as S
 import System.Process.Quick
 import System.Process.Quick.Prelude
 
 $(genCallSpec
   [TrailingHelpValidate, SandboxValidate]
   "git"
-  (   ConstArg "init"
+  (   ConstArgs (S.words "-c init.defaultBranch=master init")
   .*. ExitCodeEqualTo ExitSuccess
   .*. StdErrMatches "^$"
   .*. StdOutMatches "^Initialized empty Git repository"
